b6e152fc4b20f14013e5c42a987285666318754f
Felicio <victor_felicio@hotmail.com>
2024-08-27 21:33:22 -0300

    🐛 fix: remove strict mode to load localstorage correct

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import './style/global.scss';
 import { LibraryProvider } from './contexts/LibraryContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <LibraryProvider>
-            <App />
-        </LibraryProvider>
-    </StrictMode>
+    <LibraryProvider>
+        <App />
+    </LibraryProvider>
 );
